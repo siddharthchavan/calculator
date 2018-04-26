6b3f063e40bb080936229faaea4b5fa15d47ce45
siddharthchavan <siddharthc@alsionschool.org>
2018-04-26 11:49:38 -0700

    overhauled the algorithm

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,20 @@
 
-var num1 = prompt("term 1");
-var num2 = prompt("term 2");
+var solution = false;
+var arg1 = parseInt(prompt("first argument"));
+var arg2 = parseInt(prompt("second argument"));
+var calcFunction = prompt("calculator function: addition, subtraction, multiplication, division".toLowerCase());
 
-var solution = parseInt(num1) + parseInt(num2);
-
-document.write(solution);
+function calculate () {
+  if (calcFunction !== "addition" || "subtraction" || "multiplication" || "division") {
+    document.write("You didn't choose a function!");
+  } else if (calcFunction == "addition") {
+    solution = arg1 + arg2;
+  } else if (calcFunction == "subtraction") {
+    solution = arg1 - arg2;
+  } else if (calcFunction == "multiplication") {
+    solution = arg1 * arg2;
+  } else if (calcFunction == "division") {
+    solution = arg1 / arg2;
+  }
+  document.write(solution);
+}
